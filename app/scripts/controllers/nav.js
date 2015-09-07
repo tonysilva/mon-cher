a7158f4f5522c4ace890b83e29b60e6325acd5be
Tony Silva <tony.silva@dce.ufpb.br>
2015-09-07 14:30:16 -0300

    Including the creation date of posting

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -10,6 +10,7 @@ app.controller('NavCtrl', function ($scope, $location, Post, Auth) {
   $scope.submitPost = function () {
   	$scope.post.creator = $scope.user.profile.username;
   	$scope.post.creatorUID = $scope.user.uid;
+    $scope.post.date = new Date().toJSON();
     Post.create($scope.post).then(function (ref) {
       $location.path('/posts/' + ref.key());
       $scope.post = {url: 'http://', title: ''};
